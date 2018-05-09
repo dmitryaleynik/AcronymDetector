d9d279bffbd7ab74ec169629e910e855590433b5
Aleinik Dmitry <gerdalex39@gmail.com>
2018-05-09 15:58:04 +0300

    searching for context of abbreviation and def

--- server/acronymService/regexes.js
+++ server/acronymService/regexes.js
@@ -1,2 +1,8 @@
-const REGEXES = [/\w+\d*\w* stands for [^!^?^\.]+/g, /dsd/];
+const REGEXES = [/\w+\d*\w* stands for [^!^?^\.]+/g,
+                 /\w+\d*\w* is short for [^!^?^\.]+/ig,
+                 /\w+\d*\w* or [^!^?^\.]+/ig, 
+                 /\w+\d*\w* which is also known as [^!^?^\.]+/ig,
+                 /\w+\d*\w* an acronym for [^!^?^\.]+/ig,
+                 /\w+\d*\w* an abbreviation for [^!^?^\.]+/ig,
+                 /\w+\d*\w* which is short for [^!^?^\.]+/ig];
 module.exports = REGEXES;
--- server/acronymService/searcher.js
+++ server/acronymService/searcher.js
@@ -1,12 +1,22 @@
 const REGEXES = require('./regexes');
 function Searcher(rawText) {
-  console.log('in Searcher constractor' + rawText);
-
   this.text = rawText;
 
   this.search = function () {
-    console.log(REGEXES);
-    
+
+    var abbreviationsContext = [];
+    var promises = [];
+
+    REGEXES.forEach(function(regular, index, arr) {
+      promises.push(new Promise ((resolve, reject) => {
+        resolve(rawText.match(regular))
+      }));
+      console.log('after' + index);
+    })
+    Promise.all(promises).then(results => {
+      console.log('done');
+      console.log(results);
+    })
   }
 }
 module.exports = Searcher;
